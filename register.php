1dd2483b0003aa5b17570fc5d8896381e1c72b3a
Vipul Sharma <vipulsharma936@gmail.com>
2015-09-05 19:16:34 +0530

    Changed data

--- register.php
+++ register.php
@@ -1,10 +1,12 @@
 <?php
 $url = 'https://api.sendgrid.com/';
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
+$schoolname = $_POST['schoolname'];
+$participant = $_POST['participant'];
+$moderatorname = $_POST['moderatorname'];
+$number = $_POST['number'];
 $email = $_POST['email'];
-$message = "Data<br>Name: ".$firstname." ".$lastname."<br>Email: ".$email;
-
+$message = "Data<br>School Name: ".$schoolname." <br>Email: ".$email." <br>Teacher Moderator ".$moderatorname" <br>Phone Number: ".$number;
+//TODO add all name of participants
 $params = array(
     'api_user'  => "MegaTruck",
     'api_key'   => "#Crossktm1",
@@ -15,10 +17,7 @@ $params = array(
     'text'      => $message,
     'from'      => "register@symbiosis.idk",
   );
-
-
 $request =  $url.'api/mail.send.json';
-
 // Generate curl request
 $session = curl_init($request);
 curl_setopt ($session, CURLOPT_POST, true);
@@ -26,9 +25,7 @@ curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
 curl_setopt($session, CURLOPT_HEADER, false);
 curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
 curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-
 $response = curl_exec($session);
 curl_close($session);
-
 header("refresh:0;url=/Registers.html");
 ?>
